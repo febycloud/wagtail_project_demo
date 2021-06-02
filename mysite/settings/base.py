1b67bc117404714cee08747aac13708022bdcdc5
Fyun <fei.yun.unhi@gmail.com>
2021-06-02 13:00:18 -0400

    add fix

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -26,10 +26,8 @@ BASE_DIR = os.path.dirname(PROJECT_DIR)
 INSTALLED_APPS = [
     'home',
     'search',
-
     'article',
     'author',
-
     'wagtail.contrib.forms',
     'wagtail.contrib.redirects',
     'wagtail.embeds',
