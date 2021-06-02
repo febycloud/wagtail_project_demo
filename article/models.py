23a3ee859550be06672bd2bd7e8f0820ee14d7b7
Fyun <fei.yun.unhi@gmail.com>
2021-06-02 18:22:19 -0400

    add ref

--- article/models.py
+++ article/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.db.models.deletion import DO_NOTHING
 
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.search import index
+
+from author.models import AuthorPage
 
 # Create your models here.
 class ArticleIndexPage(Page):
@@ -15,7 +19,7 @@ class ArticleIndexPage(Page):
 class ArticlePage(Page):
     intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
-
+    author_info = models.ForeignKey(AuthorPage,blank=True,null=True,related_name='author_info',on_delete=DO_NOTHING)
     search_fields = Page.search_fields + [
         index.SearchField('intro'),
         index.SearchField('body'),
@@ -23,6 +27,7 @@ class ArticlePage(Page):
 
     content_panels = Page.content_panels + [
         FieldPanel('intro'),
+        FieldPanel('author_info'),
         FieldPanel('body', classname="full"),
     ]
 
